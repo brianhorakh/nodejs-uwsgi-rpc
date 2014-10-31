6775eadb549bd2a0acad95bae5a3a6186b87ddc9
Brian Horakh <brian.horakh@cloudbeds.com>
2014-10-31 19:15:33 +0000

    latest

--- uwsgi_rpc.js
+++ uwsgi_rpc.js
@@ -10,9 +10,48 @@ Special thanks to unbit, code ideas borrowed from:
 
 function uwsgi_rpc(addr) {
 	this.serveraddr = addr;
+	this.client = new net.Socket();
 	if (false == (this instanceof uwsgi_rpc)) {
 		return new uwsgi_rpc;
 		}
+		
+	this.client.on('data',function(data) {
+	   // console.log('DATA ' + client.remoteAddress + ': ' + data);
+		if (data.length >= 0) {
+			var pktsize = data.readUInt16LE(0);
+			// here we parse the uwsgi rpc response
+			for(var i=0;i<pktsize;i++) {
+				item_len = data.readUInt16LE(4 + i);
+				var base = 4 + i + 2;
+				var item = data.toString('utf8', base, base + item_len);
+				args.push(item);
+				i += 1 + item_len;
+				}
+			// check if the functions is defined
+			if (args.length == 0) return -1;
+			}
+		console.log("return args ---> %j", args); 
+		return(data);
+		});
+
+	this.client.on('end',function() {
+		console.log("end args ---> %j",args);
+		return(args.join("").toString("utf8"));
+		});
+
+	this.client.on('connection',function() {
+		console.log("connection");
+		});
+
+	this.client.on('close',function() {
+		console.log("close");
+		});
+
+	this.client.connect('7000','127.0.0.1',function() {
+		console.log('CONNECTED');
+		this.client.write(pkt);
+		console.log('SEND');
+		});
 	}
 module.exports = uwsgi_rpc;
 
@@ -22,8 +61,9 @@ uwsgi_rpc.prototype.serveraddr = function(addr) {
 	this.serveraddr = addr;
 	};
 
-uwsgi_rpc.prototype.call = function(args) {
-	// console.log('args: '+args.join('|'));
+uwsgi_rpc.prototype.call = function() {
+	var args = Array.prototype.slice.call(arguments,0)[1];
+	console.log('args: '+args);
 	var res = this.u_send_request(this.uwsgi_pkt(173,0,args));
 	console.log('res'+res);
 	return(res);
@@ -72,49 +112,13 @@ return $socket;
 }
 */
 
+
 uwsgi_rpc.prototype.u_send_request = function(pkt) {
 
 	var addr = this.serveraddr;
-	var client = new net.Socket();
 	var args = new Array();
+	var client = this.client;
 	
-		client.on('data',function(data) {
-		   // console.log('DATA ' + client.remoteAddress + ': ' + data);
-			if (data.length >= 0) {
-				var pktsize = data.readUInt16LE(0);
-				// here we parse the uwsgi rpc response
-				for(var i=0;i<pktsize;i++) {
-					item_len = data.readUInt16LE(4 + i);
-					var base = 4 + i + 2;
-					var item = data.toString('utf8', base, base + item_len);
-					args.push(item);
-					i += 1 + item_len;
-					}
-				// check if the functions is defined
-				if (args.length == 0) return -1;
-				}
-			console.log("return args ---> %j", args); 
-			return(data);
-			});
-
-		client.on('end',function() {
-			console.log("end args ---> %j",args);
-			return(args.join("").toString("utf8"));
-			});
-
-		client.on('connection',function() {
-			console.log("connection");
-			});
-
-		client.on('close',function() {
-			console.log("close");
-			});
-
-		client.connect('7000','127.0.0.1',function() {
-			console.log('CONNECTED');
-			client.write(pkt);
-			console.log('SEND');
-			});
 
 //		var res = u_consume_response(socket);
 //		socket.write(buf, 'utf8', function() { socket.destroy();});
