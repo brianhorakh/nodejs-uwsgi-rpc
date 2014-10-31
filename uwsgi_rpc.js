40ff5f41b7154a8c91307f648dce62439657b05c
Brian Horakh <brian.horakh@cloudbeds.com>
2014-10-31 19:25:26 +0000

    latest

--- uwsgi_rpc.js
+++ uwsgi_rpc.js
@@ -16,7 +16,7 @@ function uwsgi_rpc(addr) {
 		}
 		
 	this.client.on('data',function(data) {
-	   // console.log('DATA ' + client.remoteAddress + ': ' + data);
+	   // console.log('DATA ' + client.remoteAddress + ': ' + data)
 		if (data.length >= 0) {
 			var pktsize = data.readUInt16LE(0);
 			// here we parse the uwsgi rpc response
@@ -35,8 +35,9 @@ function uwsgi_rpc(addr) {
 		});
 
 	this.client.on('end',function() {
-		console.log("end args ---> %j",args);
-		return(args.join("").toString("utf8"));
+		console.log('end');
+		// console.log("end args ---> %j",args);
+		// return(args.join("").toString("utf8"));
 		});
 
 	this.client.on('connection',function() {
@@ -47,11 +48,6 @@ function uwsgi_rpc(addr) {
 		console.log("close");
 		});
 
-	this.client.connect('7000','127.0.0.1',function() {
-		console.log('CONNECTED');
-		this.client.write(pkt);
-		console.log('SEND');
-		});
 	}
 module.exports = uwsgi_rpc;
 
@@ -61,38 +57,39 @@ uwsgi_rpc.prototype.serveraddr = function(addr) {
 	this.serveraddr = addr;
 	};
 
-uwsgi_rpc.prototype.call = function() {
-	var args = Array.prototype.slice.call(arguments,0)[1];
-	console.log('args: '+args);
-	var res = this.u_send_request(this.uwsgi_pkt(173,0,args));
+uwsgi_rpc.prototype.exec = function() {
+	// var args = Array.prototype.slice.call(arguments,0)[0];
+	// console.log('args: '+args);
+	var res = this.u_send_request(this.uwsgi_pkt(173,0,arguments));
 	console.log('res'+res);
 	return(res);
 	}
 
 uwsgi_rpc.prototype.uwsgi_pkt = function(modifier1,modifier2) {
-		// note: this does not support arguments of type array
-		var args = Array.prototype.slice.call(arguments, 2)[0];
-		var parts = new Array;	
-		for (var i = 0; i<args.length; i++) {
-			var str = args[i].toString("utf8");
-			var itembuf = new Buffer( 2 + Buffer.byteLength(str,"utf8") );		 
-			itembuf.writeUInt16LE( Buffer.byteLength(str,"utf8"), 0);
-			itembuf.write( str, 2, "utf8");
-			parts.push(itembuf);
-			}
+	// note: this does not support arguments of type array
 
-		var body = Buffer.concat(parts);
-		var header = new Buffer(4);
-		header.fill(0,0,4);
-		header.writeUInt8(modifier1, 0);	
-		header.writeUInt16LE( body.length , 1);	
-		header.writeUInt8(modifier2, 3);	
-		parts.unshift(header);
-	
-		var r = Buffer.concat(parts);
-		// console.log("json:%j",r.toString("hex"));
-		return( r );
+	var args = Array.prototype.slice.call(arguments, 2)[0];
+	var parts = new Array;	
+	for (var i = 0; i<args.length; i++) {
+		var str = args[i].toString("utf8");
+		var itembuf = new Buffer( 2 + Buffer.byteLength(str,"utf8") );		 
+		itembuf.writeUInt16LE( Buffer.byteLength(str,"utf8"), 0);
+		itembuf.write( str, 2, "utf8");
+		parts.push(itembuf);
 		}
+
+	var body = Buffer.concat(parts);
+	var header = new Buffer(4);
+	header.fill(0,0,4);
+	header.writeUInt8(modifier1, 0);	
+	header.writeUInt16LE( body.length , 1);	
+	header.writeUInt8(modifier2, 3);	
+	parts.unshift(header);
+	
+	var r = Buffer.concat(parts);
+	// console.log("json:%j",r.toString("hex"));
+	return( r );
+	}
 
 
 /*	
@@ -118,8 +115,13 @@ uwsgi_rpc.prototype.u_send_request = function(pkt) {
 	var addr = this.serveraddr;
 	var args = new Array();
 	var client = this.client;
-	
 
+	client.connect('7000','127.0.0.1',function() {
+		console.log('CONNECTED');
+		client.write(pkt);
+		console.log('SEND');
+		});
+	
 //		var res = u_consume_response(socket);
 //		socket.write(buf, 'utf8', function() { socket.destroy();});
 		}
